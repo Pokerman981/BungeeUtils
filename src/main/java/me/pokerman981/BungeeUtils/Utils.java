f4117e3201070ec94b783d512ccfe445b666bbb1
troyg <troy.gidney@gmail.com>
2020-08-28 04:57:42 -0600

    Added a text hover to admin-chat

--- src/main/java/me/pokerman981/BungeeUtils/Utils.java
+++ src/main/java/me/pokerman981/BungeeUtils/Utils.java
@@ -3,15 +3,30 @@ package me.pokerman981.BungeeUtils;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 
 public class Utils {
 
     public static void msg(CommandSender sender, String msg) {
-        sender.sendMessage(new ComponentBuilder(ChatColor.translateAlternateColorCodes('&', msg)).create());
+        sender.sendMessage(new ComponentBuilder(color(msg)).create());
     }
 
+    public static void msg(CommandSender sender, String msg, String hoverText) {
+        TextComponent textComponent = new TextComponent(color(msg));
+        HoverEvent hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(color(hoverText)).create());
+
+        textComponent.setHoverEvent(hoverEvent);
+        sender.sendMessage(textComponent);
+    }
+
+
     public static String getText(String msg) {
-        return ChatColor.translateAlternateColorCodes('&', msg);
+        return color(msg);
+    }
+
+    public static String color(String s) {
+        return ChatColor.translateAlternateColorCodes('&', s);
     }
 
 }
--- src/main/java/me/pokerman981/BungeeUtils/commands/StaffChatCommand.java
+++ src/main/java/me/pokerman981/BungeeUtils/commands/StaffChatCommand.java
@@ -3,6 +3,7 @@ package me.pokerman981.BungeeUtils.commands;
 import me.pokerman981.BungeeUtils.Main;
 import me.pokerman981.BungeeUtils.Utils;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 public class StaffChatCommand extends Command {
@@ -19,7 +20,14 @@ public class StaffChatCommand extends Command {
         }
         if (strings.length <= 0) return;
 
-        String messageToSend = String.join(" ", strings);
+        String messageToSend = String.join(" ", strings).replace("\\", "\\\\")
+                .replace("\t", "\\t")
+                .replace("\b", "\\b")
+                .replace("\n", "\\n")
+                .replace("\r", "\\r")
+                .replace("\f", "\\f")
+                .replace("'", "\\'")
+                .replace("\"", "\\\"");
 
         Utils.msg(Main.instance.getProxy().getConsole(), Main.messages.getOrDefault("admin-chat", "Config Error!")
                 .replaceAll("%player%", commandSender.getName())
@@ -30,8 +38,9 @@ public class StaffChatCommand extends Command {
             if (proxiedPlayer.hasPermission(super.getPermission()))
                 Utils.msg(proxiedPlayer, Main.messages.getOrDefault("admin-chat", "Config Error!")
                         .replaceAll("%player%", commandSender.getName())
-                        .replaceAll("%message%", messageToSend));
+                        .replaceAll("%message%", messageToSend), ((ProxiedPlayer) commandSender).getServer().getInfo().getName());
         });
+        //  Main.instance.getProxy().getPlayer(commandSender.getName()).getServer().getInfo().getName()
 
     }
 }
